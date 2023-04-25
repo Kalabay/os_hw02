5ebe485c78356efd43d71f731938aa596881642a
mikalabay <mikalabay@edu.hse.ru>
2023-04-25 07:42:36 +0300

    Create task5.c

---
+++ task5.c
@@ -0,0 +1,128 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) { // fisrt arg - cnt_proc
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    int cnt_proc = strtoll(argv[1], NULL, 10);
+
+    if (cnt_proc <= 0) {
+        return 0;
+    }
+
+    int *cnt_call = mmap(NULL, cnt_proc, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (cnt_call == MAP_FAILED) {
+        perror("memory failed");
+        exit(1);
+    }
+
+    pid_t *sons = malloc(cnt_proc * sizeof(sons));
+    sem_t **sem = malloc(cnt_proc * sizeof(*sem));
+
+    for (int i = 0; i < cnt_proc; ++i) {
+        sem[i] = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        if (sem[i] == MAP_FAILED) {
+            perror("sem create");
+            exit(1);
+        }
+        if (sem_init(sem[i], 1, 0) == -1) {
+            perror("sem_init");
+            exit(1);
+        }
+    }
+    for (int i = 0; i < cnt_proc; ++i) {
+        cnt_call[i] = 0;
+    }
+
+    for (int i = 0; i < cnt_proc; ++i) {
+        sons[i] = fork();
+        if (!sons[i]) {
+            srand(i);
+            int state = rand() % 2;
+            if (!state) { // if it waits call
+                sem_post(sem[i]);
+            }
+            while (1) {
+                if (state) { // it wants to call
+                    printf("%d %s", i, "будет звонить\n");
+                    int want_to_call = rand() % cnt_proc;
+                    int check = -1;
+                    while (want_to_call == i || check == -1) {
+                        want_to_call = rand() % cnt_proc;
+                        if (want_to_call != i) {
+                            check = sem_trywait(sem[want_to_call]);
+                        }
+                    }
+                    cnt_call[i] += 1;
+                    cnt_call[want_to_call] += 1;
+                    printf("%d %s %d %s", i, "позвонил", want_to_call, "\n");
+                    // TALKING
+                    sleep(10);
+                    printf("%d %s %d %s", i, "завершился", want_to_call, "\n");
+                    // TALKING
+                    // call finished
+                } else { // it waits for call
+                    printf("%d %s", i, "будет ждать звонка\n");
+                    int own_value;
+                    while (1) { // process of waiting call
+                        sem_getvalue(sem[i], &own_value);
+                        if (own_value == 0) {
+                            // TALKING
+                            printf("%d %s", i, "болтает\n");
+                            sleep(10);
+                            printf("%d %s %s", i, "завершился", "\n");
+                            // TALKING
+                            break; // stop waiting call
+                        }
+                    }
+                }
+
+                // change state
+                state = rand() % 2;
+                if (!state) { // if it waits for call
+                    sem_post(sem[i]); // increase semaphore
+                }
+            }
+        }
+    }
+
+    // time for program execution
+    int exec_time = 15;
+    sleep(exec_time);
+    // time for program execution
+
+    for (int i = 0; i < cnt_proc; i++) {
+        kill(sons[i], SIGKILL);
+        wait(NULL);
+    }
+    for (int i = 0; i < cnt_proc; ++i) {
+        sem_close(sem[i]);
+    }
+
+    free(sem);
+    free(sons);
+
+    for (int i = 0; i < cnt_proc; ++i) {
+        printf("%d cnt: %d \n", i, cnt_call[i]);
+    }
+    munmap(cnt_call, cnt_proc);
+
+    for (int i = 0; i < cnt_proc; ++i) {
+        sem_destroy(sem[i]);
+        munmap(sem[i], sizeof(sem_t));
+    }
+    return 0;
+}
